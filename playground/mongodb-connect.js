90ac730690e6f390a0848b0782027ffc86216567
Richard Duan <richardduan@192-168-1-10.tpgi.com.au>
2017-10-11 23:20:25 +1100

    find

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,9 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
 
 MongoClient.connect('mongodb://localhost:37000/TodoApp', (err, db)=>{
   if(err) {
     console.log('Unable to connect mongodb on 37000');
   }
 
-  console.log('connect to mongo on 37000')
+  console.log('connect to mongo on 37000');
+
+  // db.collection('Todos').insertOne({text: 'something'},(err, result)=>{
+  //   if(err) {
+  //     return console.log('unable to insert todo', err);
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'jack wang',
+    age: 20,
+    location: 'sydney'
+  }, (err, result)=>{
+      if(err) {
+      return console.log('unable to insert todo', err);
+    }
+    console.log(result);
+    console.log(JSON.stringify(result.ops, undefined, 2));
+    console.log(result.ops[0]._id.getTimestamp());
+  })
+
+  db.close();
+
 });
---
+++ playground/mongodb-find.js
@@ -0,0 +1,37 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:37000/TodoApp', (err, db)=>{
+  if(err) {
+    console.log('Unable to connect mongodb on 37000');
+  }
+
+  console.log('connect to mongo on 37000');
+
+  //find all documents
+  db.collection('Todos').find().toArray().then((docs)=>{
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err)=>{
+    console.log(err);
+  })
+
+  db.collection('Todos').find().count().then((count)=>{
+    console.log(count);
+  }, (err)=>{
+    console.log(err);
+  });
+
+  //_id is not a string, it is an ObjectID object
+  db.collection('Todos').find({
+    _id: new ObjectID('59ddedaa251c0f7e8eb3e9dc')
+  })
+    .toArray().then((docs)=>{
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err)=>{
+    console.log(err);
+  })
+
+  // db.close();
+
+});
