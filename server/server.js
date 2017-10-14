ce5ec78fc13bca57b8c20844995374e852f0ada7
Richard Duan <richardduan@192-168-1-6.tpgi.com.au>
2017-10-14 21:45:06 +1100

    get

--- server/server.js
+++ server/server.js
@@ -21,12 +21,19 @@ app.post('/todos', (req, res) => {
 
     res.status(200).send(doc);
   }, (err) => {
-    // console.log(err);
+    console.log(err);
     res.status(400).send(err);
   })
 });
 
-// app.get('/')
+app.get('/todos', (req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (err)=>{
+    console.log(err);
+    res.status(400).send(err);
+  })
+});
 
 app.listen(3000, () => {
   console.log('on 3000');
@@ -34,27 +41,5 @@ app.listen(3000, () => {
 });
 
 
-// var newTodo = new Todo({
-//   text: 'cooking',
-//   // completed: false,
-//   // completedAt: 123
-
-// });
-
-// newTodo.save().then((doc)=>{
-//   console.log('Saved', doc)
-// }, (err)=>{
-//   console.log(err);
-// });
-
-
-
-// var newUser = new User({email: 'user@ab1.com'});
-
-// newUser.save().then((user)=>{
-//   console.log(user);
-// }, (err)=>{
-//   console.log(err);
-// })
 
 module.exports = app;
